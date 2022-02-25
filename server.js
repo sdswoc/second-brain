f7a045dba14b813e35eeee697a7eb117c219080c
Akshit Kumar Gautam <akshitkumargautam@gmail.com>
2022-02-26 00:55:59 +0530

    Trying implementation of authetication in a different folder and then will implement it in the main app

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
-require("dotenv").config();
 const express = require("express");
-const status = require("statuses");
-const jwt = require("jsonwebtoken");
+const mongoose=require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
+// app.use("/", express.static(path.join(__dirname, "static")));
+app.use(bodyParser.json());
 
-const posts = [
-  {
-    username: "Anne Marie",
-    title: "Rockabye",
-  },
-  {
-    username: "Sandra",
-    title: "Chameleon",
-  },
-];
-
-app.get("/posts", (req, res) => {
-  res.json(posts);
-  res.json(status);
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+  res.send({ status: "ok" });
 });
-
-app.post("/login", (req, res) => {
-  // Authentication of user
-
-  const username = req.body.username;
-  const user = { name: username };
-
-  const accessToken = process.env.ACCESS_TOKEN_SECRET;
-  res.json({ accessToken: accessToken });
-});
-
-app.listen(3000);
