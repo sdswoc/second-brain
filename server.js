b92cde1a56c490285a22743ad4b430b172e1884d
Akshit Kumar Gautam <akshitkumargautam@gmail.com>
2022-03-09 11:40:09 +0530

    Forgot to swtich to new branch

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
-    res.render('index.ejs')
-})
+// GET Routes
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
 
 app.listen(port, () => {
-    console.log(`Server up on Port ${port}...`);
+  console.log(`Server up on Port ${port}...`);
 });
